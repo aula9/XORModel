3bc982cfa3e96734ed361c6cf5e6a99f22c1e871
Aula Jazmati <37100066+aula9@users.noreply.github.com>
2023-10-20 09:08:34 -0700

    Create XorMLPClassifier.py

---
+++ XorMLPClassifier.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sklearn.neural_network
+inputs = np.array([[0,0],[0,1],[1,0],[1,1]])
+expected_output = np.array([0,1,1,0])
+
+model = sklearn.neural_network.MLPClassifier(
+                activation='logistic',
+                max_iter=100,
+                hidden_layer_sizes=(2,),
+                solver='lbfgs')
+model.fit(inputs, expected_output)
+print('predictions:', model.predict(inputs))
+print(model.score(inputs,expected_output ))
